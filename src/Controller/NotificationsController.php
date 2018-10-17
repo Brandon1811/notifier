fc6a1cceecd080c89fef59006df9a527a8f2b0b6
BSounder <brandon.shipley@exactindustries.com>
2018-10-16 21:10:37 -0700

    added read controller wrapper for component mark as read

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -12,15 +12,41 @@ use Cake\I18n\Time;
  *
  * @property \App\Model\Table\NotificationsTable $Notifications
  *
- * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ * @method \App\Model\Entity\Notification[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
 class NotificationsController extends AppController
 {
+    /**
+	 * @var array
+	 */
+	public $components = [
+		'Bakkerij/Notifier.Notifier',
+	];
+
     /**
 	 * @return \Cake\Http\Response|null
 	 */
 	public function index()
     {
-        Log::write("debug", 'inside index notifications controller');
+        $notifications = $this->Notifier->getNotifications();
+        $this->set(compact('notifications'));
+    }
+
+    /**
+     * @return \Cake\Http\Response|null
+     */
+    public function read($id = null)
+    {
+        $this->request->allowMethod(['post', 'put']);
+        $unreadCountBefore = $this->Notifier->countNotifications(null, true); // original unread count
+        $plural = (isset($id)) ? 'notification':'notifications';
+        if ($this->request->is(['post', 'put'])) {
+            $this->Notifier->markAsRead($id);
+        }
+        if ($unreadCountBefore === $this->Notifier->countNotifications(null, true)) {
+            $this->Flash->error('Failed to mark ' . $plural . 'as read. Please, try again.');
+        } else {
+            $this->Flash->success('Marked ' . $plural . ' as read.');
+        }
     }
 }
